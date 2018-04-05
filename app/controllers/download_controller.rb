808870b4172c2899729d34c96f5c7be00f8775f4
Yuriy Alekseyev <dev.yuriy.a@gmail.com>
2018-04-05 14:59:36 +0300

    Add basic controllers functionality

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,4 +1,25 @@
 class DownloadController < ApplicationController
   def index
+    @downloads = Download
+                 .where(user_id: current_user.id)
+                 .order(:created_at)
+  end
+  def show
+    if params[:id]
+      @download = Download.find_by(id: params[:id])
+    end
+
+    render 'download'
+  end
+
+  def new
+    render 'download'
+  end
+
+  def destroy
+    @download = Download.find(params[:id])
+    @download.destroy
+
+    redirect_to action: :index
   end
 end
--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -1,4 +1,26 @@
 class StreamController < ApplicationController
   def index
+    @streams = Stream
+                 .where(user_id: current_user.id)
+                 .order(:created_at)
+  end
+
+  def show
+    if params[:id]
+      @stream = Stream.find(params[:id])
+    end
+
+    render 'stream'
+  end
+
+  def new
+    render 'stream'
+  end
+
+  def destroy
+    @stream = Stream.find_by(id: params[:id])
+    @stream.destroy
+
+    redirect_to action: :index
   end
 end
