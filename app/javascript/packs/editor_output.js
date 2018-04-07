7e27cb7a5f3c05200d595d8ee3b8700d1a34e0da
Yuriy Alekseyev <dev.yuriy.a@gmail.com>
2018-04-07 16:37:57 +0300

    Set editor for input and output, readonly mode: true

--- app/javascript/packs/editor_output.js
+++ app/javascript/packs/editor_output.js
@@ -4,10 +4,12 @@ import 'brace/theme/monokai';
 
 var editor_output = ace.edit('editor-output');
 editor_output.getSession().setMode('ace/mode/javascript');
+editor_output.setReadOnly(true);
 editor_output.setTheme('ace/theme/monokai');
 editor_output.setFontSize('14px');
 
 var editor_input = ace.edit('editor-input');
 editor_input.getSession().setMode('ace/mode/javascript');
+editor_input.setReadOnly(true);
 editor_input.setTheme('ace/theme/monokai');
 editor_input.setFontSize('14px');
