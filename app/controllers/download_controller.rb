0c276a3e6b0719b35d9026967a1ae51daeb8ed81
Yuriy Alekseyev <dev.yuriy.a@gmail.com>
2018-04-08 15:53:15 +0300

    Add .prettify to Download model

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,32 +1,14 @@
 class DownloadController < ApplicationController
   def index
-    @downloads = Download
-                 .where(user_id: current_user.id)
-                 .order(created_at: :desc)
-  end
-
-  def history
-    @download = Download.find_by(id: params[:id])
-
-    render 'download/history'
+    @downloads = User.find(current_user.id).downloads
   end
 
   def show
-    @download = Download.find_by(id: params[:id])
-    @input = Input.where(download_id: @download.id)
-    @output = Output.where(download_id: @download.id)
-    @errors = OutputError.select(:message).where(download_id: @download.id).distinct
-    render 'download'
-  end
-
-  def new
-    render 'download'
-  end
-
-  def destroy
     @download = Download.find(params[:id])
-    @download.destroy
-
-    redirect_to action: :index
+    @stream = Stream.find(@download.stream_id)
+    @input = Download.prettify(@download.inputs)
+    @output = Download.prettify(@download.outputs)
+    @errors = @download.output_errors.select(:message).distinct
+    render 'download'
   end
 end
--- app/models/download.rb
+++ app/models/download.rb
@@ -3,6 +3,13 @@ class Download < ApplicationRecord
 
   has_many :inputs, dependent: :destroy
   has_many :outputs, dependent: :destroy
+  has_many :output_errors, dependent: :destroy
 
   validates :name, presence: true
+
+  def self.prettify(arr)
+    arr = arr.map { |el| JSON.parse(el.content) }
+
+    JSON.pretty_generate(arr.flatten)
+  end
 end
